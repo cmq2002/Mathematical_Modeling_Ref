12bb64197069225d3c238bd7173560ecfc93f215
Otter J <114224792+lhmqk@users.noreply.github.com>
2022-11-24 19:41:48 +0700

    Update note.m

--- Code_Ex2/note.m
+++ Code_Ex2/note.m
@@ -1,24 +1,23 @@
+format rational;
 lamda1_FORMULA=@(a, b, c, d) (a + d - sqrt(4.*b.*c + (a.^2) - 2.*a.*d + d.^2))/(2);
 lamda2_FORMULA=@(a, b, c, d) (a + d + sqrt(4.*b.*c + (a.^2) - 2.*a.*d + d.^2))/(2);
 RReal_FORMULA=@(a, b, R0, J0, lamda1, lamda2) ( ((a.*R0 + b.*J0 - lamda2.*R0)/(lamda1 - lamda2)).*exp(lamda1) ) - ( ((b.*J0 + (a - lamda1).*R0)/(lamda1 - lamda2)).*exp(lamda2) );
 JReal_FORMULA=@(a, b, R0, J0, lamda1, lamda2) ((lamda1 - a).*((a.*R0 + b.*J0 - lamda2.*R0)/(b.*(lamda1 - lamda2))).*exp(lamda1)) - ((lamda2 - a).*((b.*J0 + R0.*(a - lamda1))/(b.*(lamda1 - lamda2))).*exp(lamda2));
-a=[7 2 2];
-b=[2 7 2];
-c=[-2 -7 3];
-d=[-7 -2 -3];
-R0=[1.35 1.35 1.5];
-J0=[-14 -7 12];
-lamda1=[0 0 0];
-lamda2=[0 0 0];
-R=[0 0 0];
-J=[0 0 0];
-str=["EagerBeaver_Hermit" "EagerBeaver_Hermit" "EagerBeaver_CautiousLover"];
+a=      [7      2       2       1       1       1       -2      -5      -5      -5];
+b=      [2      7       2       1       1       -1      5       2       1       1];
+c=      [-2     -7      3       -2      -2      -2      -5      -2      -1      -3];
+d=      [-7     -2      -3      5       1       -0.5    2       5       -2      -5];
+R0=     [1.35   1.35    1.5     1.25    1.35    1.35    1.35    1.35    1.35    1.35];
+J0=     [-14    -7      12      1       1       1       5       5       -5      -5];
+lamda1= [0 0 0 0 0 0 0 0 0 0];
+lamda2= [0 0 0 0 0 0 0 0 0 0];
+R=      [0 0 0 0 0 0 0 0 0 0];
+J=      [0 0 0 0 0 0 0 0 0 0];
+str=["EagerBeaver_Hermit" "EagerBeaver_Hermit" "EagerBeaver_CautiousLover" "EagerBeaver_NarcissisticNerd" "EagerBeaver_NarcissisticNerd" "NarcissisticNerd_Hermit" "NarcissisticNerd_CautiousLover" "NarcissisticNerd_CautiousLover" "CautiousLover_Hermit" "CautiousLover_Hermit"];
 
-for i = 1.0:+1.0:3.0
-   lamda1(i)=sym(lamda1_FORMULA(a(i), b(i), c(i), d(i)))
-   lamda2(i)=sym(lamda2_FORMULA(a(i), b(i), c(i), d(i)))
-end
-for i = 1.0:+1.0:3.0
+for i = 1.0:+1.0:10.0
+   lamda1(i)=lamda1_FORMULA(a(i), b(i), c(i), d(i))
+   lamda2(i)=lamda2_FORMULA(a(i), b(i), c(i), d(i))
    disp(str(i))
    disp('lamda:')
    disp(sym(lamda1(i)))
